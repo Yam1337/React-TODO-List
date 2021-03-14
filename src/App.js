cf2a0d52f32526f6972ef5df1ebda4c71b0229f3
Rafal Medryk <yam1337@gmail.com>
2021-03-14 18:09:04 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  let [myTasks, setMyTasks] = React.useState([]);
+  let [myValue, setMyValue] = React.useState("");
+  let inputValue = (e) => {
+    setMyValue(e.target.value);
+    //console.log(myValue);
+  };
+  let deleteTask = (e) => {
+    setMyTasks(myTasks.filter((x) => x.id !== e));
+  };
+  let addTask = () => {
+    if (myValue.length >= 1) {
+      setMyTasks([...myTasks, { value: myValue, id: Math.random() }]);
+      setMyValue("");
+      console.log(myTasks, myTasks.id);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">TODO-List</div>
+      <div className="content">
+        <div className="box1">
+          <div className="text">
+            <input
+              className="myInput"
+              onChange={inputValue}
+              value={myValue}
+              maxlength="100"
+              placeholder="max 100 letters"
+            />
+            <button className="myButton1" onClick={addTask}>
+              Add
+            </button>
+          </div>
+        </div>
+        <div className="box2">
+          {
+            <div className="text">
+              {myTasks.map((x) => (
+                <div className="tasks">
+                  <ul>
+                    <li>
+                      <div class="taskval">{x.value}</div>
+                    </li>
+                  </ul>
+                  <button
+                    className="myButton2"
+                    onClick={() => {
+                      deleteTask(x.id);
+                    }}
+                  >
+                    X
+                  </button>
+                </div>
+              ))}
+            </div>
+          }
+        </div>
+      </div>
     </div>
   );
 }
